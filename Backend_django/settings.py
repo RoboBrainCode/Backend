197837da73aaa5dc8e0682b4ba5930ec46962cb0
Debarghya Das <dd367@cornell.edu>
2014-09-11 19:32:45 -0400

    _ozan -> _br

--- Backend_django/settings.py
+++ Backend_django/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'backend_ozan',                      # Or path to database file if using sqlite3.
+        'NAME': 'backend_br',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
