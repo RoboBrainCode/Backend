d664dc01a4902182bebdccaa4b2c460d0c339914
deedydas <dd367@cornell.edu>
2014-09-19 04:31:56 +0000

    ready to deploy on test

--- Backend/settings.py
+++ Backend/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'backend_dev_django',                      # Or path to database file if using sqlite3.
+        'NAME': 'backend_br',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -39,7 +39,7 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
+SITE_ID=u'541b56c54249914c94ddf443'
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
--- manage.py
+++ manage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import uwsgi
-from uwsgidecorators import timer
-from django.utils import autoreload
-
-@timer(3)
-def change_code_gracefull_reload(sig):
-    if autoreload.code_changed():
-	 uwsgi.reload()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
