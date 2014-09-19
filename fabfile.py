2c82c095e4ca1f18cec8289f360c647a193f665a
Debarghya Das <dd367@cornell.edu>
2014-09-19 07:55:49 -0400

    fab

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,16 @@
-from fabric.api import cd, env, local, settings, run
+from fabric.api import cd, env, local, settings, run, sudo
 
 def prod():
   env.host_string = 'ec2-54-218-14-187.us-west-2.compute.amazonaws.com'
-  env.user = 'ubuntu'
   env.key_filename = 'conf/www.pem'
 
 def test():
   env.host_string = 'ec2-54-218-20-10.us-west-2.compute.amazonaws.com'
-  env.user = 'ubuntu'
   env.key_filename = 'conf/www.pem'
 
-def deploy():
+def deploy(user='ubuntu'):
+  env.user = user
   with cd('/var/www/Backend'):
     run('git checkout master')
     run('git pull')
-    run('sudo uwsgi --reload /tmp/robobrain-master.pid')
+    sudo('uwsgi --reload /tmp/robobrain-master.pid')
