8e347edb555cea6ad76096f8ce7bffdd7f5ff513
Debarghya Das <dd367@cornell.edu>
2014-09-19 08:21:26 -0400

    push

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,25 @@
 from fabric.api import cd, env, local, settings, run, sudo
 
-def prod():
+def prod_deploy(user='ubuntu'):
   env.host_string = 'ec2-54-218-14-187.us-west-2.compute.amazonaws.com'
   env.key_filename = 'conf/www.pem'
+  env.user = user
+  with cd('/var/www/Backend'):
+    run('git checkout test')
+    run('git pull')
+    run('git checkout production')
+    run('git merge test')
+    sudo('uwsgi --reload /tmp/robobrain-master.pid')
 
-def test():
+def test_deploy(user='ubuntu'):
   env.host_string = 'ec2-54-218-20-10.us-west-2.compute.amazonaws.com'
   env.key_filename = 'conf/www.pem'
-
-def deploy(user='ubuntu'):
   env.user = user
   with cd('/var/www/Backend'):
     run('git checkout master')
     run('git pull')
+    run('git checkout test')
+    run('git pull')
+    run('git merge master')
+    run('git push')
     sudo('uwsgi --reload /tmp/robobrain-master.pid')
