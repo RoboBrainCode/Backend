d73f8443a23d325ef1a4cad284b719d909065398
deedydas <dd367@cornell.edu>
2014-10-10 03:30:57 +0000

    adding

--- Backend/settings.py
+++ Backend/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': 'ec2-54-186-47-107.us-west-2.compute.amazonaws.com',# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'ec2-54-186-42-131.us-west-2.compute.amazonaws.com',# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '27017',                      # Set to empty string for default.
     }
 }
