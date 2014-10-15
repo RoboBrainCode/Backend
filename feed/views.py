aa784a378b20160949aaf80a96b7ced233eba9e0
Debarghya Das <dd367@cornell.edu>
2014-10-14 23:09:39 -0400

    backend ordering and save aggregation fix'

--- feed/views.py
+++ feed/views.py
@@ -5,6 +5,7 @@ import numpy as np
 from django.core import serializers
 import dateutil.parser
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.db.transaction import commit_on_success
 
 # Returns k most recent feeds from BrainFeed table.
 def return_top_k_feeds(request):
@@ -13,17 +14,23 @@ def return_top_k_feeds(request):
 
     max_len = ViewerFeed.objects.count()
     upper_limit = min(max_len, top_k)
-    feed_ids = ViewerFeed.objects.values_list('feedid', flat=True).order_by('id')[:upper_limit]
-    brainfeeds = BrainFeeds.objects.filter(id__in=list(feed_ids))
 
-    for feeds in brainfeeds:
-        feeds.update_score = True
-        feeds.log_normalized_feed_show += 1.0
-        feeds.save()
+    feed_ids = list(ViewerFeed.objects.values_list('feedid', flat=True).order_by('id')[:upper_limit])
 
+    brainfeeds_db = BrainFeeds.objects.filter(id__in=feed_ids)
+
+    # Reordering brainfeeds from the DB in order of feed_ids in O(n)
+    # s.t. feed_ids == [bf.id for bf in brainfeeds]
+    feed_map_order = {feed_ids[i] : i for i in xrange(len(feed_ids))}
+    brainfeeds = [0]  * len(feed_ids)
+    for bf in list(brainfeeds_db):
+        brainfeeds[feed_map_order[bf.id]] = bf
+
+    update_scores_top_k(brainfeeds)
     json_feeds = [feed.to_json() for feed in brainfeeds]
 
     return HttpResponse(json.dumps(json_feeds), content_type="application/json")
+
 
 # This function allows infinite scrolling.
 def infinite_scrolling(request):
@@ -36,18 +43,37 @@ def infinite_scrolling(request):
 
     max_len = ViewerFeed.objects.count()
     upper_limit = min(max_len, current_feeds + extra_feeds)
-    feed_ids = ViewerFeed.objects.values_list('feedid', flat=True).order_by('id')[current_feeds:upper_limit]
-    brainfeeds = BrainFeeds.objects.filter(id__in=list(feed_ids))
 
+    feed_ids = list(ViewerFeed.objects.values_list('feedid', flat=True).order_by('id')[current_feeds:upper_limit])
+
+    brainfeeds_db = BrainFeeds.objects.filter(id__in=feed_ids)
+
+    # Reordering brainfeeds from the DB in order of feed_ids in O(n)
+    # s.t. feed_ids == [bf.id for bf in brainfeeds]
+    feed_map_order = {feed_ids[i] : i for i in xrange(len(feed_ids))}
+    brainfeeds = [0]  * len(feed_ids)
+    for bf in list(brainfeeds_db):
+        brainfeeds[feed_map_order[bf.id]] = bf
+
+    update_scores_scroll(brainfeeds, current_feeds, extra_feeds)
+    json_feeds = [feed.to_json() for feed in brainfeeds]
+
+    return HttpResponse(json.dumps(json_feeds), content_type="application/json")
+
+@commit_on_success
+def update_scores_top_k(brainfeeds):
+    for feeds in brainfeeds:
+        feeds.update_score = True
+        feeds.log_normalized_feed_show += 1.0
+        feeds.save()
+
+@commit_on_success
+def update_scores_scroll(brainfeeds, current_feeds, extra_feeds):
     page_number = current_feeds/max(1.0,extra_feeds) + 1.0
     for feeds in brainfeeds:
         feeds.update_score = True
         feeds.log_normalized_feed_show += np.log10(1.0+page_number)
         feeds.save()
-
-    json_feeds = [feed.to_json() for feed in brainfeeds]
-
-    return HttpResponse(json.dumps(json_feeds), content_type="application/json")
 
 # Filters feeds using the hash word
 def filter_feeds_with_hashtags(request):
@@ -137,8 +163,8 @@ def upvotes_recorder(request):
             if change:
                 brain_feed.upvotes -= 1
         votes = {
-            'upvotes': brain_feed.upvotes,
-            'downvotes': brain_feed.downvotes
+            'upvotes': max(brain_feed.upvotes, 0),
+            'downvotes': max(brain_feed.downvotes, 0)
         }
         brain_feed.save()
 
