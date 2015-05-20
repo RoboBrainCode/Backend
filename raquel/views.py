da185e1fed19f2266425abd327943321d2552662
SiddhantManocha <siddhantmanocha1994@gmail.com>
2015-05-20 15:52:42 -0400

    edited raquel api

--- raquel/views.py
+++ raquel/views.py
@@ -5,7 +5,11 @@ import json
 from Raquel.raquel import *
 def rachQuery(request):
 	val=unicodedata.normalize('NFKD', dict(request.GET)['query'][0]).encode('ascii','ignore')
-	val=val.strip()
+	query=val.strip()
+	val=unicodedata.normalize('NFKD', dict(request.GET)['qtype'][0]).encode('ascii','ignore')
+	qtype=val.strip()
+	print query
+	print qtype
 	return HttpResponse(json.dumps({'success':1}), content_type="application/json")
 
 
