05057cf7b933ab37f2817dd6bef4e28356b0de06
SiddhantManocha <siddhantmanocha1994@gmail.com>
2015-05-28 20:13:24 -0400

    modified graph backend

--- graph/views.py
+++ graph/views.py
@@ -1,4 +1,3 @@
-#bp Create your views here.
 from django.http import HttpResponse
 import json
 from threading import Lock
@@ -6,36 +5,14 @@ from weaverGetNode import *
 clientLock=Lock()
 import unicodedata
 def getNode(request):
-	val=unicodedata.normalize('NFKD', dict(request.GET)['query'][0]).encode('ascii','ignore')
-	val=val.strip()
-
+	val=unicodedata.normalize('NFKD', dict(request.GET)['query'][0]).encode('ascii','ignore').strip()
 	number=int(unicodedata.normalize('NFKD', dict(request.GET)['number'][0]).encode('ascii','ignore'))
-	print val
-	print number
 	overwrite=int(unicodedata.normalize('NFKD', dict(request.GET)['overwrite'][0]).encode('ascii','ignore'))
-
 	direction=str(unicodedata.normalize('NFKD', dict(request.GET)['directionVal'][0]).encode('ascii','ignore'))
 
-	lines=""
-	# with open('/home/siddhantmanocha/intern/code/Intern/roboBrain/Backend/graph/arctic.json') as f:
-	# 	for line in f:
-	# 		lines=lines+line.strip()
 	with clientLock:
-		result1,result2=getNodeEdge(name=val,num=number,overwrite=overwrite,directionVal=direction)
-	#result1['nodes'][0]['root']='true'
-	list1=result1['nodes']
-        result=json.dumps(result1)
-	import os.path
-	Filename=os.path.abspath(os.path.join('./', os.pardir))+'/Frontend/app/sample_data/arctic.json'
-	with open(Filename,'w') as f:
-		f.write(result)
+		result=getNodeEdge(name=val,num=number,directionVal=direction)
 
-	result2['nodes'][0]['root']='true'
-#	for i in result1['nodes']:
- #               if i['handle'][0]==val:
-  #                      i['root']='true'
-#			print 'Root added'
- #                       break
+	result['nodes'][0]['root']='true'
 
-	# return HttpResponse(json.dumps(lines), content_type="application/json")
-	return HttpResponse(json.dumps(result2), content_type="application/json")
+	return HttpResponse(json.dumps(result), content_type="application/json")
--- graph/weaverGetNode.py
+++ graph/weaverGetNode.py
@@ -1,105 +1,61 @@
 import hashlib
 from weaver import client
 import json
-c = client.Client('172.31.33.213', 2002) # Syslab
-# c = client.Client('128.84.167.248', 2002) # Arpit's
-# c = client.Client('127.0.0.1', 2002) # Syslab
+c = client.Client('172.31.33.213', 2002)
+
 def _get_unique_id(node_handle):
     m = hashlib.md5(node_handle.encode())
     return str(str(int(m.hexdigest(), 16))[0:16])
 
-def getNodeEdge(name='phone',num=5,overwrite=1,directionVal='F'):
-    retVal=dict()
-    retVal1=dict()
-    if not overwrite:
-        lines=""
-	import os.path
-	Filename=os.path.abspath(os.path.join('./', os.pardir))+'/Frontend/app/sample_data/arctic.json'
-#	print Filename        
-	with open(Filename,'r') as f:
-            for line in f:
-                lines=lines+line
-        retVal=dict(json.loads(lines))
-	retVal1['nodes']=retVal['nodes']
-	retVal1['edges']=list()
+def ProcessNodeData(dataObj,name):
+    data=dict(dataObj)
+    data['id']=name
+    data['caption']=name
+    if data['labels'][0][1:8]=='Concept':
+        data['type']='Concept'
     else:
-        retVal['nodes']=list()
-        retVal['edges']=list()
-        retVal1['nodes']=list()
-        retVal1['edges']=list()
+        data['type']='Media'
+    return data
 
+def ProcessEdgeData():
+    props=dict()
+    if directionVal=='B':
+        props['source']=edge.end_node
+        props['target']=edge.start_node
+    else:
+        props['source']=edge.start_node
+        props['target']=edge.end_node
+
+    props['type']=edge.properties['label'][0]
+    props['handle']=edge.handle
+
+
+def getNodeEdge(name='phone',num=5,directionVal='F'):
+    retVal=dict(),retVal['nodes']=list(),retVal['edges']=list(),nodeList=list()
     nodename=name
     onehop=c.traverse(nodename).out_edge({'edgeDirection':directionVal}).node().execute()
-#    print onehop
-    nodeList=list()
-    for nodeDict in retVal['nodes']:
-        print '1:',nodeDict['caption']
-        nodeList.append(nodeDict['caption'])
-    edgeList=list()
-    for edgeDict in retVal['edges']:
-        edgeList.append(edgeDict['source']+','+edgeDict['target'])
-
-    if not name in nodeList:
-        nodeOb=c.get_node(node=name)
-        data=dict(nodeOb.properties)
-        data['id']=_get_unique_id(name)
-        data['caption']=name
-	if data['labels'][0][1:8]=='Concept':
-		data['type']='Concept'
-	else:
-		data['type']='Media'
-        retVal['nodes'].append(data)
-	retVal1['nodes'].append(data)
-        nodeList.append(name)
-    
+    nodeOb=c.get_node(node=name)
+    retVal['nodes'].append(ProcessNodeData(nodeOb.properties,name))
     counter=0
-    for node in onehop:
-        if not node in nodeList:
-            print '2:',node
-            counter=counter+1
-            nodeOb=c.get_node(node=node)
-            data=dict(nodeOb.properties)
-            data['id']=_get_unique_id(node)
-            data['caption']=node
-	    if data['labels'][0][1:8]=='Concept':
-                data['type']='Concept'
-            else:
-                data['type']='Media'
+    nodeList.append(name)
 
-            retVal['nodes'].append(data)
-            retVal1['nodes'].append(data)
-            nodeList.append(node)
-            if counter==num:
-                break
+    for node in onehop:
+        counter=counter+1
+        nodeOb=c.get_node(node=node)
+        data=dict(nodeOb.properties)
+        retVal['nodes'].append(ProcessData(nodeOb.properties,node))
+        nodeList.append(node)
+        if counter==num:
+            break
 
     edges=c.get_edges(node=nodename,properties=[('edgeDirection',directionVal)])
+
     for edge in edges:
-        props=dict()
         if edge.start_node in nodeList and edge.end_node in nodeList:
-	    insertVal=""
-	    if directionVal=='F':
-		insertVal=(edge.start_node+','+edge.end_node)
-	    else:
-		insertVal=(edge.end_node+','+edge.start_node)
+            retVal['edges'].append(ProcessEdgeData(edge))
 
-            if insertVal not in edgeList:
-		edgeList.append(insertVal)
-                if directionVal=='B':
-                    props['source']=_get_unique_id(edge.end_node)
-                    props['target']=_get_unique_id(edge.start_node)
-                else:
-                    props['source']=_get_unique_id(edge.start_node)
-                    props['target']=_get_unique_id(edge.end_node)
-
-                props['type']=edge.properties['label'][0]
-		print edge.handle
-                #props['id']=(edge.handle)
-		props['handle']=edge.handle
-		print props['source'],props['target']
-                retVal['edges'].append(props)
-		retVal1['edges'].append(props)
-    print len(retVal['edges'])
-    return retVal,retVal1
+    return retVal
+        
 
 def getNode(name='phone'):
     nodename=name
