db34ffe4b68e66ab88bcc8b284267b43a0ef04c1
asheshjain399 <asheshjain399@gmail.com>
2016-04-21 13:39:20 +0000

    minor

--- Backend/settings.py
+++ Backend/settings.py
@@ -15,11 +15,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'robobrain_backend',                      # Or path to database file if using sqlite3.
+        'NAME': 'backend_test_deploy',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '54.68.112.33',# ec2-54-148-208-139.us-west-2.compute.amazonaws.com Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '52.39.63.201',# ec2-54-148-208-139.us-west-2.compute.amazonaws.com Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': 27017,                      # Set to empty string for default.
     }
 }
--- Backend/settings_production.py
+++ Backend/settings_production.py
@@ -5,11 +5,12 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'robobrain_backend',                      # Or path to database file if using sqlite3.
+        'NAME': 'backend_test_deploy',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': 'ec2-52-37-226-117.us-west-2.compute.amazonaws.com',
+        'HOST': '52.39.63.201',
+        #'ec2-54-68-112-33.us-west-2.compute.amazonaws.com',
     #    ec2-54-148-208-139.us-west-2.compute.amazonaws.com',# Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
        'PORT': '27017',                      # Set to empty string for default.
     }
